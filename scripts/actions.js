5286958ad0bd4d65bf8bf84d708ab0883cf663c8
Sandeep <59565935+54nd339@users.noreply.github.com>
2021-02-02 03:34:24 +0530

    Add files via upload

---
+++ scripts/actions.js
@@ -0,0 +1,159 @@
+// lifeTime 祯数
+const color1 = {
+  h: 279,
+  s: '100%',
+  l: '50%',
+  a: '80%'
+};
+const color2 = {
+  h: 197,
+  s: '100%',
+  l: '50%',
+  a: '80%'
+};
+const color3 = {
+  h: 0,
+  s: '100%',
+  l: '50%',
+  a: '80%'
+};
+const points1 = [];
+const points2 = [];
+
+
+const Actions = [
+  {
+    lifeTime: 60,
+    texts: [{
+      text: '3',
+      hsla: color1
+    }]
+  },
+
+  {
+    lifeTime: 90,
+    func: (width, height) => {
+      if (!points1.length) {
+        for (let i = 0; i < 1200; i++) {
+          let x = (i - 1200 / 2) / 300;
+          let y = Math.sqrt(Math.abs(x)) - Math.sqrt(Math.cos(x)) * Math.cos(30 * x);
+          if (!isNaN(y)) {
+            points1.push({ x, y })
+          }
+        }
+      }
+
+      const p = points1[~~(Math.random() * points1.length)]
+      const radius = Math.min(width * 0.4, height * 0.4);
+      return {
+        x: p.x * radius / 2,
+        y: p.y * radius / 2,
+        z: ~~(Math.random() * 30),
+        color: {
+          h: 0,
+          s: '100%',
+          l: '70%',
+          a: '80%'
+        }
+      };
+    }
+  },
+
+  {
+    lifeTime: 60,
+    texts: [{
+      text: '2',
+      hsla: color1
+    }]
+  },
+
+  {
+    lifeTime: 90,
+    func: (width, height) => {
+      if (!points1.length) {
+        for (let i = 0; i < 1200; i++) {
+          let x = (i - 1200 / 2) / 300;
+          let y = Math.sqrt(Math.abs(x)) - Math.sqrt(Math.cos(x)) * Math.cos(30 * x);
+          if (!isNaN(y)) {
+            points1.push({ x, y })
+          }
+        }
+      }
+
+      const p = points1[~~(Math.random() * points1.length)]
+      const radius = Math.min(width * 0.4, height * 0.4);
+      return {
+        x: p.x * radius / 2,
+        y: p.y * radius / 2,
+        z: ~~(Math.random() * 30),
+        color: {
+          h: 0,
+          s: '100%',
+          l: '70%',
+          a: '80%'
+        }
+      };
+    }
+  },
+
+  {
+    lifeTime: 60,
+    texts: [{
+      text: '1',
+      hsla: color1
+    }]
+  },
+
+  {
+    lifeTime: 180,
+    texts: [
+      {
+        text: 'Sandy',
+        hsla: color2
+      },
+      {
+        text: ' ❤️ ',
+        hsla: color3
+      },
+      {
+        text: 'Saanj',
+        hsla: color2
+      },
+    ]
+  },
+
+  {
+    lifeTime: 180,
+    texts: [
+      {
+        text: 'I',
+        hsla: color2
+      },
+      {
+        text: ' ❤️ ',
+        hsla: color3
+      },
+      {
+        text: 'U',
+        hsla: color2
+      },
+    ]
+  },
+]
+
+setTimeout(function () {
+    let bg_music_src = $('.bg_music').data('src');
+    $(".bg_music").append(`<audio src="happybirthday.mp3" autoplay="autoplay" loop id="bg_music"></audio>`);
+    let audio = document.getElementById('bg_music');
+    
+    document.addEventListener("WeixinJSBridgeReady", function () {
+      audio.play();
+    }, false);
+  }, 200)
+
+// https://www.w3schools.com/jsref/met_audio_play.asp
+function click_event() {
+  console.log("clicked");
+  let audio = document.getElementById('bg_music');
+  audio.play();
+}
